bc911cb7cd8754042582658b2c3e5cc2563fa558
Nicolas Panozo <nicopanozoc@gmail.com>
2024-10-29 22:38:53 -0400

    The component for contact information was created and integrated to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className="p-8">
         <About />
         <Projects />
+        <Contact />
       </main>
     </div>
   );
