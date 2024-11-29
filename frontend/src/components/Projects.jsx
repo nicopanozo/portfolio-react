3cfddf82861ea985d2968f6ae033f5a7fd519b40
Nicolas Panozo <nicopanozoc@gmail.com>
2024-11-28 23:38:22 -0400

    Add Ollama tools project to Projects.jsx

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -4,6 +4,7 @@ import ProjectCard from './ProjectCard';
 import project1Image from '../assets/images/project1.jpg';
 import project2Image from '../assets/images/project2.jpg';
 import phpStore from '../assets/images/phpStore.png';
+import ollamaTools from '../assets/images/ollamaTools.png';
 import { motion } from "framer-motion";
 
 const Projects = () => {
@@ -20,16 +21,16 @@ const Projects = () => {
         <h2 className="text-3xl font-bold text-gray-800 mb-6">Proyectos</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <ProjectCard
-            title="Manejo de inventario en PHP con MySQL"
-            description="Sistema de manejo de inventario en PHP con MySQL. Sistema CRUD completo para manejo de productos, stock, ventas y usuarios."
+            title="PHP, Yii2 and MySQL Stock Management Web App for Clothing Store"
+            description="PHP application using Yii and MySQL to streamline retail operations. Features include user management, product catalog, sales tracking, and real-time inventory updates."
             imageSrc={phpStore}
             projectLink="https://github.com/nicopanozo/tienda-php.git"
           />
           <ProjectCard
-            title="Proyecto 2"
-            description="Descripción breve del proyecto 2."
-            imageSrc={project1Image}
-            projectLink="https://enlace-al-proyecto2.com"
+            title="Ollama AI: functions as tools for LLM’s"
+            description="This AI project provides a comprehensive guide and code examples for leveraging Ollama's function calling feature. Discover how to extend the capabilities of your language models by allowing them to interact with external tools and data sources."
+            imageSrc={ollamaTools}
+            projectLink="https://github.com/nicopanozo/ollama-functions-as-tools.git"
           />
           <ProjectCard
             title="Proyecto 2"
